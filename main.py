606cab7e32dcf447ec22b1a3e59d595d0177c676
NetanMangal <imnetanmangal@gmail.com>
2021-05-29 23:10:54 +0530

    read subdomains from file

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import argparse
+import sys
 
-def getDomainFromCL():
+SUBDOMAINS_FILENAME = "subdomains.txt"
+
+def get_domain_from_cl():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', help='target domain', dest='domain', default="")
     args = parser.parse_args()
     return args.domain
 
-# with open("./subdomains.txt") as f:
-#     for line in f:
-#         print(line.strip())
+def perform_brute_force(domain):
+    with open(SUBDOMAINS_FILENAME) as f:
+        for line in f:
+            print(line.strip() + '.' + domain)
 
 def main():
     # get domain from command line using -d option
-    domain = getDomainFromCL()
-    print(domain)
-    
+    domain = get_domain_from_cl()
+
+    if (not domain):
+        print("Parent domain not specified using -d flag.")
+        sys.exit(1)
+
+    perform_brute_force(domain)
+
 main()
