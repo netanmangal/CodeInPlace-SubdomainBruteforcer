4d29fb5724b3f9c8ce89e0d9883df83f3937bd1a
NetanMangal <imnetanmangal@gmail.com>
2021-05-29 23:02:13 +0530

    take domain as argument

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', help='target domain', dest='domain', default="")
-args = parser.parse_args()
-domain = args.domain
+def getDomainFromCL():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', help='target domain', dest='domain', default="")
+    args = parser.parse_args()
+    return args.domain
 
-print(domain)
+# with open("./subdomains.txt") as f:
+#     for line in f:
+#         print(line.strip())
+
+def main():
+    # get domain from command line using -d option
+    domain = getDomainFromCL()
+    print(domain)
+    
+main()
