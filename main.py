493ea6794e40c3294e986708520bb47dd390e4cc
NetanMangal <imnetanmangal@gmail.com>
2021-05-29 23:19:02 +0530

    colored outputs

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import sys
 
 SUBDOMAINS_FILENAME = "subdomains.txt"
 
+C_RESET = "\033[0m"
+C_RED = "\033[1;31m"
+C_GREEN = "\033[1;32m"
+C_YELLOW = "\033[1;33m"
+C_WHITE = "\033[1;37m"
+
 def get_domain_from_cl():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', help='target domain', dest='domain', default="")
@@ -12,7 +18,13 @@ def get_domain_from_cl():
 def perform_brute_force(domain):
     with open(SUBDOMAINS_FILENAME) as f:
         for line in f:
-            print(line.strip() + '.' + domain)
+            resolve_subdomain(line.strip() + '.' + domain)
+
+def resolve_subdomain(subdomain):
+    print(f"{C_RED}[-] {subdomain} {C_RESET}")
+
+    print(f"{C_YELLOW}[!] {subdomain} {C_RESET}")
+    print(f"{C_GREEN}[+] {subdomain} {C_RESET}")
 
 def main():
     # get domain from command line using -d option
